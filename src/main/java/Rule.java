e0d01f1cea1c382316d30a56b8abee428108eccc
tehmischi <michael.annen@gmx.net>
2022-09-26 02:29:14 +0200

    changed output of negative variables to - instead of n

--- src/main/java/Rule.java
+++ src/main/java/Rule.java
@@ -6,8 +6,16 @@ public record Rule(Literal head, List<Literal> body) {
     public String toString() {
         String headString;
         StringBuilder bodyString = new StringBuilder();
-        headString = head + " <- ";
-        body.forEach(x -> bodyString.append(x).append(" & "));
+        String headVariable = head.toString();
+        boolean negativeHead = headVariable.startsWith("n");
+        headString = negativeHead?headVariable.replaceFirst("n", "-"):headVariable;
+        headString += " <- ";
+        body.forEach(bodyLiteral -> {
+            String bodyLiteralString = bodyLiteral.toString();
+            boolean isNegative = bodyLiteralString.startsWith("n");
+            String returnString = isNegative?bodyLiteralString.replaceFirst("n", "-"):bodyLiteralString;
+            bodyString.append(returnString).append(" & ");
+        });
         int bodySize = bodyString.length();
         return headString + bodyString.substring(0, bodySize - 2);
     }
